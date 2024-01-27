dac60c3341793caedadd5f377db4cf70d7dee90b
tiagohlazarinidev <tiagolazarinidev@gmail.com>
2024-01-27 17:33:51 -0300

    feat: added helper tools

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 import soma from "../soma";
 
 describe("Soma", () => {
-  test("Deve somar 1 + 1 = 2", () => {
-    expect(soma(1, 2)).toBe(3);
-  });
+	test("Deve somar 1 + 1 = 2", () => {
+		expect(soma(1, 2)).toBe(3);
+	});
 });
